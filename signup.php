67fccb82c35bd8845ce2f0da083b2faa8768467e
gulshanydv <159876931+gulshanydv@users.noreply.github.com>
2024-03-08 17:25:10 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -16,7 +16,7 @@ session_start();
 
 			//save to database
 			$user_id = random_num(20);
-			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
+			$query = "insert into users (user_id,user_name,password,date) values ('$user_id','$user_name','$password',NOW())";
 
 			mysqli_query($con, $query);
 
