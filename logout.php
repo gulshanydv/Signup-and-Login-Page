9f1ba1ffd58f4646311e343f358316a7af1e6511
gulshanydv <159876931+gulshanydv@users.noreply.github.com>
2024-03-08 15:25:42 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,26 @@
+<?php
+
+// session_start();
+
+// if(isset($_SESSION['user_id']))
+// {
+// 	unset($_SESSION['user_id']);
+
+// }
+
+// header("Location: login.php");
+// die;
+?>
+
+
+
+<?php
+session_start();
+
+if(isset($_SESSION['user_id']))
+{
+    unset($_SESSION['user_id']);
+}
+header("Location: login.php");
+die;
+?>
